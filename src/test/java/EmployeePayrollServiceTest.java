a7a4498329a5c423788e352b71e3a165ec7a2849
Rekhajambhulkar <jambhulkarrekha@gmail.com>
2020-12-25 21:13:25 +0530

    Ability to retrieve Employee join in Given date range

--- src/test/java/EmployeePayrollServiceTest.java
+++ src/test/java/EmployeePayrollServiceTest.java
@@ -37,6 +37,16 @@ public class EmployeePayrollServiceTest {
         Assert.assertTrue(result);
     }
 
-
+    @Test
+    public void givenDateRange_WhenRetrieved_ShouldMatchEmployeeCount() {
+        EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+        employeePayrollService.readEmployeePayrollData(EmployeePayrollService.IOService.DB_IO);
+        LocalDate startDate = LocalDate.of(2018, 01, 01);
+        LocalDate endDate = LocalDate.now();
+        List<EmployeePayrollData> employeePayrollData =
+                employeePayrollService.readEmployeePayrollForDateRange(EmployeePayrollService
+                        .IOService.DB_IO, startDate, endDate);
+        Assert.assertEquals(3, employeePayrollData.size());
+    }
 
 }
